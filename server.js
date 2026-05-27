9b59ba4683c82c127fde82028bd2527ab10266c7
code-with-ameen <contact.developer.ameen@gmail.com>
2026-05-27 20:11:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ app.post('/contact', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on", PORT);
 });
